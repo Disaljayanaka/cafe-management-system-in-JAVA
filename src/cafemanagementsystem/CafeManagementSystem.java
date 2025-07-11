b9abc23cc884383d2d84ef83867788ad85034bf3
Disaljayanaka <disaljayanaka26@gmail.com>
2025-07-11 16:48:52 +0530

    connect login part

--- src/cafemanagementsystem/CafeManagementSystem.java
+++ src/cafemanagementsystem/CafeManagementSystem.java
@@ -15,6 +15,7 @@ public class CafeManagementSystem {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        new login().setVisible(true);
     }
     
 }
